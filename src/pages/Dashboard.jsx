46a55c6970973009162af1c25ac109c35e9aa545
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-11 00:15:46 +0530

    pages

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Logout from "../components/auth/Logout";
+
+const Dashboard = () => (
+  <div>
+    <h2>Dashboard</h2>
+    <Logout />
+  </div>
+);
+
+export default Dashboard;
